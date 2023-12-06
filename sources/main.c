a9b234684fac2567a6e861336f11bee9630d456f
Sebastien Vidot <svidot@c2r4p3.42perpignan.fr>
2023-12-06 16:16:10 +0100

    test execve ok

--- sources/main.c
+++ sources/main.c
@@ -6,33 +6,67 @@
 /*   By: svidot <svidot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 11:14:52 by svidot            #+#    #+#             */
-/*   Updated: 2023/12/06 14:09:21 by svidot           ###   ########.fr       */
+/*   Updated: 2023/12/06 16:04:24 by svidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
 
-void assign_files(int argc, char *argv[], char *filepaths[])
+void	set_filepaths(int argc, char **argv[], char *filepaths[])
 {
-	filespath[0] = argv[1];
-	filespath[1] = argv[--argc];
+	filepaths[0] = *(++*argv);
+	filepaths[1] = (*argv)++[argc - 2];
+}
+
+void	set_cmd(char **cmd_arr, char *argv[])
+{
+	int	n_cmd;
+	
+	n_cmd = -1;	
+	while (*argv++)
+		n_cmd++;
+	//malloc
+	ft_printf("filepath2:%d\n", n_cmd);
+}
+
+void	child_area()
+{
+	
+}
+
+void	parent_area()
+{
+	
 }
 
 int	main(int argc, char *argv[], char *envp[])
 {
 	char	*filepaths[2];
 	
-	char	*cmd1;
-	char	*cmd2;
+	char	**cmd_arr;
 	
 	if (argc <= 4)
 		return (1);
-	assign_filepaths(argc, argv, filepaths);
+	set_filepaths(argc, &argv, filepaths);
+	//set_cmd(cmd_arr, argv);
+	ft_printf("argv:%s\n", *argv);
+	ft_printf("filepath1:%s\n", filepaths[0]);
+	ft_printf("filepath2:%s\n", filepaths[1]);	
 	
-	cmd1 = *++argv;
-	
-	cmd2 = *++argv;
-	ft_printf("argv:%s\n", *(argv + 1));
-	ft_printf("envp:%s\n", *envp);
+	if (execve("/bin/ls", (char *[]) {"echo", "-l", NULL}, envp))
+		perror("error execve");
+	ft_printf("tu ne me verra jamais\n");
+	// pid_t pid= fork();
+	// if (pid < 0)
+	// 	perror("error pid");
+	// else if (pid == 0)
+	// 	child_area();
+	// else
+	// 	parent_area();
 	return (0);
 }
