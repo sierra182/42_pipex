a496fc874f56de3bf9d594b009ea87799052d96f
sierra182 <sierra-echo@msn.com>
2023-12-26 23:00:46 +0100

    wip pandoras parsing

--- sources/parsing_pandorasbox.c
+++ sources/parsing_pandorasbox.c
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 09:06:02 by svidot            #+#    #+#             */
-/*   Updated: 2023/12/22 09:50:00 by seblin           ###   ########.fr       */
+/*   Updated: 2023/12/25 20:05:21 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 typedef enum e_tok
 {
+	SQUTE,
+	DQUTE,
 	CMD,
 	ARG_CMD,
 	OPT_CMD,
@@ -39,11 +41,37 @@ t_ast_node	*create_node(t_tok token)
 	new_node->token = token;
 	return (new_node);
 }
+int	set_squte_nde(t_ast_nde squte, char *argv)
+{
+	while (*argv)
+	{
+		if (*argv == '\'')
+		{
+			if (!cmd->start)
+				cmd->start = argv;
+			else
+			{
+				cmd->end = argv;
+				break ;
+			}
+		}
+		argv++;
+	}
+	if (cmd->start && !cmd->end)
+	{
+		ft_putstr_fd("simple quote error.\n", 2); // gerer err
+		return (-1);
+	}
+	else if (!cmd->start && !cmd->end)
+		return (0);
+	else if (cmd->start && cmd->end)
+		return (1);
+}
 
 void	set_cmd_nde(t_ast_nde cmd, char *argv)
 {
 	while (*argv)
-	{		
+	{
 		while (*argv && !ft_isspace(*argv))
 		{
 			if (!cmd->start)
@@ -61,6 +89,17 @@ void	set_cmd_nde(t_ast_nde cmd, char *argv)
 
 void	set_token(t_ast_nde seek, char *argv)
 {
+	if (set_squte_nde(seek, argv))
+	{
+		seek->token = SQUTE;
+		seek->child = create_node(CMD);
+	}
+	else
+	{
+		seek->token = 0;
+		seek->start = NULL;	
+		seek->end = NULL;	
+	}
 	seek->token = CMD;
 	set_cmd_nde(seek, argv);
 	seek->child = create_node();
